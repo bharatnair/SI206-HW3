4c053bf7f6e28cc353b6b6fbfd4a8ad82c03d433
Bharat Nair <BharatNair@0587380523.wireless.umich.net>
2016-11-07 16:23:38 -0500

    finished this part and removed my personal keys

---
+++ twitterhw3b_no_pass.py
@@ -0,0 +1,54 @@
+# In this assignment you must do a Twitter search on any term
+# of your choice.
+# Deliverables:
+# 1) Print each tweet
+# 2) Print the average subjectivity of the results
+# 3) Print the average polarity of the results
+
+# Be prepared to change the search term during demo.
+
+import tweepy
+from textblob import TextBlob
+
+# Unique code from Twitter
+access_token = "SOME KEY"
+access_token_secret = "SOME KEY"
+consumer_key = "SOME KEY"
+consumer_secret = "SOME KEY"
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
+# public_tweets = api.search(q = 'trivandrum')
+public_tweets = api.search(input("Enter a term you'd like to search on Twitter: "))
+
+subj_sum = 0
+subj_count = 0 
+pol_sum = 0
+pol_count = 0
+
+# print (type(public_tweets))
+print ("\n**** start ****\n")
+
+for tweet in public_tweets:
+	print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	print(analysis.sentiment)
+	subj_sum += analysis.sentiment.subjectivity
+	pol_sum += analysis.sentiment.polarity
+	subj_count +=1
+	pol_count +=1
+	print ()
+
+print ()
+if len(public_tweets) == 0:
+	print ("Oops, this search term returned zero results on Twitter!")
+else:
+	print("Average subjectivity: " + str(subj_sum/subj_count))
+	print("Average polarity: " + str(pol_sum/pol_count))
+print ()
+
+print ("\n**** end ****\n")
